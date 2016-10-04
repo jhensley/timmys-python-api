0ff3041eaa8169c9faa68a0794f523d3ac892e1c
Jonathan Hensley <jhensley@gannett.com>
2016-10-04 12:32:28 -0400

    added helpers

--- coins/views.py
+++ coins/views.py
@@ -2,6 +2,16 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
+# We're using Foobar Currency, where there are 100 Foos to one Bar and
+# the denominations are:
+#	 1  Foo
+#    5  Foos
+#    10 Foos
+#    20 Foos
+#    50 Foos
+#    1  Bar
+#    2  Bars
+
 @csrf_exempt
 def api(request):
     response = {}
